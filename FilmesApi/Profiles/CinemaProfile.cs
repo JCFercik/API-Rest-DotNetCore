6a98a7c49dc4bd8e764e58ab202c02b58166caac
JCFercik <jc.fercikferreira@gmail.com>
2022-03-14 22:16:44 -0300

    Criação da estrutura DTO's para classe cinema

---
+++ FilmesApi/Profiles/CinemaProfile.cs
@@ -0,0 +1,16 @@
+﻿using FilmesApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace FilmesApi.Profiles
+{
+    public class CinemaProfile : Profile
+    {
+        CreateMap<CreateCinemaDto, Cinema>();
+        CreateMap<Cinema, ReadCinemaDto>();
+        CreateMap<UpdateCinemaDto, Cinema>();
+    }
+}
